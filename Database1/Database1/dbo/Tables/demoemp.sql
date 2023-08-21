869a4c4028360adecd0888c80382200ec5d38ec5
jobincyriac54 <54598249+jobincyriac54@users.noreply.github.com>
2023-08-21 11:33:03 +0530

    Test

--- Database1/Database1/dbo/Tables/demoemp.sql
+++ Database1/Database1/dbo/Tables/demoemp.sql
@@ -3,6 +3,6 @@
     [employee_name]   VARCHAR (MAX) NULL,
     [employee_salary] INT           NULL,
     [employee_age]    INT           NULL,
-    [profile_image]   VARCHAR (MAX) NULL
+    [profile_image]   VARCHAR (MAX) NOT NULL
 );
 
